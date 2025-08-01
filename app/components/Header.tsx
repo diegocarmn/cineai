a75f8ebacea32c838588d7de26571f8ab0ea38bd
Diego Carmona <diegoncarmona@gmail.com>
2025-08-01 00:30:25 -0300

    ✨ Initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 m-2 p-4 rounded-full">
+        <h1 className="text-white">CineIA</h1>
+    </header>
+  );
+};
+
+export default Header;
