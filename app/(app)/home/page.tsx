93f529f0bfe1bb84d49d62b78a86d6bdabdc1ced
Diego Carmona <diegoncarmona@gmail.com>
2025-08-02 15:29:44 -0300

    feat(ui): add user name custom welcome message at /Home

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,21 +1,22 @@
-import Button from "../../components/Button";
-import { LuSearch } from "react-icons/lu";
-import { FaSearch } from "react-icons/fa";
-import { IoSearch } from "react-icons/io5";
 
-export default function HomePage() {
+import Button from "../../components/Button";
+import { IoSearch } from "react-icons/io5";
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
+
+export default async function HomePage() {
+  const session = await auth();
   return (
     <div className="flex flex-col items-center justify-center">
       <h1
         className="
-          text-3xl font-bold font-heading text-transparent 
+          text-3xl font-bold font-heading text-transparent text-center
           bg-gradient-to-r from-yellow-50 via-yellow-100 to-yellow-200 
           bg-clip-text sm:text-5xl md:text-7xl
         "
       >
-        Welcome to CineAI
+        Welcome to CineAI, <br /> {session.user.name}
       </h1>
-      <p className="pt-2 text-body text-base text-center text-balance sm:text-lg md:text-xl">
+      <p className="pt-2 text-body text-base text-center text-balance sm:text-lg md:text-xl md:w-auto">
         Search for movies and shows, add to your list, and get recommendations
         based on what you like!
       </p>
